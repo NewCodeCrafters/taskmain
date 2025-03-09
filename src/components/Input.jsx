52f5991fdcca6c053f4934a0f576ab196be8c6ce
gideon-del <gideonchidi471@gmail.com>
2025-03-09 22:28:50 +0100

    chore: fixes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,7 +8,21 @@ const TextInput = ({
   error,
   ...inputProps
 }) => {
-  return <div>{label && <label>{label}</label>}</div>;
+  return (
+    <div>
+      {label && (
+        <label className="body-small-medium text-neutral-black-13">
+          {label}
+        </label>
+      )}
+      <div
+        className="px-3 py-2 rounded-full border border-neutral-black-5  shadow-sm shadow-[#1678F2]  drop-shadow-sm  "
+        tabIndex={0}
+      >
+        <input type="text" />
+      </div>
+    </div>
+  );
 };
 
 export default TextInput;
